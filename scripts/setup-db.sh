b66b81558dfd7ec27587998c3ffa53928146ce07
Ryota Egusa <egusa.ryota@gmail.com>
2019-08-27 12:24:19 +0900

    Set up db

---
+++ scripts/setup-db.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ ! -z "$1" ] && [ $1 != '-y' ]; then
+    echo "unknown option: $1"
+    exit 1
+fi
+
+docker-compose exec pg psql -c 'create database toshokan;' -U postgres
+
+docker-compose build
+if [ $? -gt 0 ]; then
+    exit $?
+fi
+
+docker-compose run --rm ridgepole -c env:DATABASE_URL --apply --dry-run
+if [ $? -gt 0 ]; then
+    exit $?
+fi
+
+while [ -z "$1" ]; do
+    read -p 'Are you sure you want to apply changes? [Y/n]' ans
+    case $ans in
+        [Y] ) break;;
+        [Nn] ) exit;;
+        * ) echo "Input [Y/n]";;
+    esac
+done
+
+docker-compose run --rm ridgepole -c env:DATABASE_URL --apply
